0a395482e5a71d9dc3e0b2ed60bf2b7b394573e8
Nathan Wilcox <nejucomo@gmail.com>
2015-02-20 21:42:34 -0800

    Rename rust crate to "sappho"; implement a barebones peg parser which is exercised in main().

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![feature(plugin)]
+#![plugin(peg_syntax_ext)]
+
 fn main() {
-    println!("Hello, world!");
+    print_parse("foo");
 }
+
+fn print_parse(src: &str) {
+    println!("Source: {}", src);
+    match parser::expression(src) {
+        Ok(v) => println!("Ok({})", v),
+        Err(e) => println!("Err({})", e),
+    }
+}
+
+peg_file! parser("sappho.rustpeg");
