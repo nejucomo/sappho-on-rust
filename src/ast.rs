29d7a23b1762e636810e5b824df44d7bead60bc0
Nathan Wilcox <nejucomo@gmail.com>
2015-03-03 11:20:50 -0800

    Now that I realize the types ``self`` and ``mut self`` allow move of self and the peg parser supports that, we don't need PropBuilder.

--- src/ast.rs
+++ src/ast.rs
@@ -105,42 +105,10 @@ impl Properties {
 
         Properties { map: m, varprop: vp }
     }
-}
 
-pub enum PropBuilder {
-    Cell(PropItem, Box<PropBuilder>),
-    Tail(Option<PropItem>),
-}
+    pub fn plus_item(mut self, id: Identifier, expr: Expression) -> Properties {
+        self.map.insert(id, Box::new(expr));
 
-impl PropBuilder {
-    pub fn new_from_varprop(id: Identifier, expr: Expression) -> PropBuilder {
-        PropBuilder::Tail(Some((id, Box::new(expr))))
-    }
-
-    pub fn add_item(self, id: Identifier, expr: Expression) -> PropBuilder {
-        let item = (id, Box::new(expr));
-        PropBuilder::Cell(item, Box::new(self))
-    }
-
-    pub fn as_properties(self) -> Properties {
-        let mut m = HashMap::new();
-        let mut cell = self;
-
-        loop {
-            match cell {
-                PropBuilder::Cell((id, expr), nextcell) => {
-                    m.insert(id, expr);
-                    cell = *nextcell;
-                }
-                PropBuilder::Tail(vp) => {
-                    return Properties {
-                        map: m,
-                        varprop: vp,
-                    }
-                }
-            }
-        }
+        self
     }
 }
-
-
