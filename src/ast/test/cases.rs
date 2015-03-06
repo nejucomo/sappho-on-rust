e5bd96ace618c7f73a6c5f8d125bf5a7bc8df389
Nathan Wilcox <nejucomo@gmail.com>
2015-03-06 15:25:34 -0800

    Define syntax-based mutation/indeterminism restrictions; these are verified post-parsing to simplify the grammar.

--- src/ast/test/cases.rs
+++ src/ast/test/cases.rs
@@ -3,6 +3,10 @@ use super::super::parse_verified_expression;
 use super::framework::{
     // see $crate::ast::test::framework for test_parse_expectations! macro.
     expr,
+    papp,
+    proc_return,
+    qapp,
+    query,
 };
 
 
@@ -12,5 +16,31 @@ test_parse_expectations! {
 
     smokecheck
         : &["true"]
-        => Some(expr(true))
+        => Some(expr(true));
+
+    // procapp is invalid in D/Q contexts and only valid in P contexts:
+    invalid_procapp_in_deterministic_context
+        : &["!true"]
+        => None;
+
+    invalid_procapp_in_query_context
+        : &["query -> !true"]
+        => None;
+
+    valid_procapp_in_proc_context
+        : &["proc { return !true }"]
+        => Some(expr(proc_return(papp(true))));
+
+    // queryapp is invalid in D contexts and only valid in Q/P contexts:
+    invalid_queryapp_in_deterministic_context
+        : &["$true"]
+        => None;
+
+    valid_queryapp_in_proc_context
+        : &["proc { return $true }"]
+        => Some(expr(proc_return(qapp(true))));
+
+    valid_queryapp_in_query_context
+        : &["query -> $true"]
+        => Some(expr(query(qapp(true))))
 }
