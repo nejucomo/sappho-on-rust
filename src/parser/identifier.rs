e3cbf68cf602743c835db51bbceacbaba8a06a12
Nathan Wilcox <nejucomo@gmail.com>
2016-08-25 16:52:37 -0700

    Implement keyword checking in the identifier parser so that it passes existing tests.

--- src/parser/identifier.rs
+++ src/parser/identifier.rs
@@ -1,18 +1,37 @@
-use combine::{Stream, ParseResult};
+use combine::ParseResult;
 
 
-pub fn identifier<I>(input: I) -> ParseResult<String, I>
-    where I: Stream<Item = char>
+pub fn identifier(input: &str) -> ParseResult<String, &str>
 {
-    use combine::{Parser, ParserExt};
+    use combine::{Parser, ParserExt, parser};
     use combine::char::{alpha_num, char, letter};
     use combine::combinator::many;
+    use parser::keywords::KEYWORDS;
 
     let head = letter().or(char('_'));
     let tail = alpha_num().or(char('_'));
 
     (head, many(tail))
         .map(|t: (char, String)| t.0.to_string() + &t.1)
+        .then(|id| parser(move |input| {
+            use combine::primitives::{Consumed, Error, ParseError, StreamOnce};
+
+            let _: &str = input; // Require &str as input type.
+
+            if !KEYWORDS.contains(&id.as_str()) {
+                Ok((id.clone(), Consumed::Empty(input)))
+            } else {
+                let position = input.position();
+                let err = ParseError::new(
+                    position,
+                    Error::Message(
+                        format!(
+                            "expected identifer, found keyword {:?}",
+                            id)
+                        .into()));
+                Err((Consumed::Empty(err)))
+            }
+        }))
         .parse_state(input)
 }
 
---
+++ src/parser/keywords.rs
@@ -0,0 +1,10 @@
+pub const KEYWORDS: [&'static str; 8] = [
+    "𝜆",
+    "\\",
+    "proc",
+    "query",
+    "let",
+    "in",
+    "from",
+    "bind",
+    ];
