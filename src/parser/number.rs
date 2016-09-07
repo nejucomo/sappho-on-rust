05ecce20a921167ebee22cd9835840a4a6e12cd1
Nathan Wilcox <nejucomo@gmail.com>
2016-09-07 15:54:55 -0700

    Improve test failure diagnostics for number parsing then begin to implement case.zero parser.

--- src/parser/number.rs
+++ src/parser/number.rs
@@ -4,13 +4,29 @@ use value::Number;
 
 pub fn number(input: &str) -> ParseResult<Number, &str>
 {
+    use combine::{Parser, ParserExt, char, parser};
+
+    char('+')
+        .with(parser(signless_number))
+        .or(char('-')
+            .with(parser(signless_number))
+            .map(|n| -n))
+        .or(parser(signless_number))
+        .parse_state(input)
+}
+
+
+pub fn signless_number(input: &str) -> ParseResult<Number, &str>
+{
     use combine::{Parser, ParserExt, digit, many1};
-    use num::{BigRational, Num};
 
     many1(digit())
         .and_then(
             |s: String| {
-                BigRational::from_str_radix(s.as_str(), 10)
+                use num::{BigInt, BigRational, Num};
+
+                BigInt::from_str_radix(s.as_str(), 10)
+                    .map(BigRational::from_integer)
                     .map(Number::from_bigrational)
             })
         .parse_state(input)
@@ -20,6 +36,7 @@ pub fn number(input: &str) -> ParseResult<Number, &str>
 #[cfg(test)]
 mod tests {
     use super::number;
+    use value::Number;
 
     macro_rules! include_number {
         ($case_name:expr) => {
@@ -38,7 +55,6 @@ mod tests {
                 #[test]
                 fn $case_name() {
                     use std::iter::FromIterator;
-                    use combine::{Parser, ParserExt, eof, parser};
 
                     let inputs: Vec<&str> = Vec::from_iter(
                         include_cases!(
@@ -48,18 +64,8 @@ mod tests {
 
                     for i in 0..inputs.len() {
                         for j in (i+1)..inputs.len() {
-                            let ares = parser(number).skip(eof()).parse(inputs[i]);
-                            let bres = parser(number).skip(eof()).parse(inputs[j]);
-
-                            assert!(ares.is_ok());
-                            assert!(bres.is_ok());
-
-                            let (a, arem) = ares.unwrap();
-                            let (b, brem) = bres.unwrap();
-
-                            assert_eq!(arem, "");
-                            assert_eq!(brem, "");
-
+                            let a = test_parse_number(inputs[i]);
+                            let b = test_parse_number(inputs[j]);
                             assert_eq!(a, b);
                         }
                     }
@@ -69,4 +75,15 @@ mod tests {
     }
 
     test_cases_number_parser!(zero);
+
+    fn test_parse_number(input: &str) -> Number {
+        use combine::{Parser, ParserExt, eof, parser};
+
+        let res = parser(number).skip(eof()).parse(input);
+        assert!(res.is_ok(), "Failed to parse: {:?}", input);
+        let (x, rem) = res.unwrap();
+        assert_eq!(rem, "");
+
+        x
+    }
 }
--- src/value/number.rs
+++ src/value/number.rs
@@ -1,14 +1,20 @@
+use std::ops::Neg;
 use num::BigRational;
 
 
 #[derive(Debug)]
 #[derive(PartialEq)]
 #[derive(PartialOrd)]
-pub enum Number {
-    RBig(BigRational),
-}
+pub struct Number(BigRational);
 
 
 impl Number {
-    pub fn from_bigrational(r: BigRational) -> Number { Number::RBig(r) }
+    pub fn from_bigrational(r: BigRational) -> Number { Number(r) }
+}
+
+
+impl Neg for Number {
+    type Output = Number;
+
+    fn neg(self) -> Number { Number(-self.0) }
 }
