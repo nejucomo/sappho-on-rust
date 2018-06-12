9b4c0a3c58e5682af80cab43571e6194f8a932af
Nathan Wilcox <nathan@z.cash>
2018-06-12 10:20:46 -0700

    Add some number parsing and a homebrew Number value type. (Yikes!)

--- src/parser/number.rs
+++ src/parser/number.rs
@@ -1,24 +1,40 @@
-use combine::{ParseResult};
+use combine::ParseResult;
 use value::Number;
-use num::BigRational;
+
+
+macro_rules! from_radix {
+    ($t:ty, $radix:expr) => {
+        |s: String| <$t>::from_str_radix(s.as_str(), $radix)
+    }
+}
+
+
+macro_rules! signed {
+    ($p:expr) => {
+        {
+            use combine::{ParserExt, char};
+
+            char('+')
+                .with($p)
+                .or(char('-')
+                    .with($p)
+                    .map(|n| -n))
+                .or($p)
+        }
+    }
+}
 
 
 pub fn number(input: &str) -> ParseResult<Number, &str>
 {
-    use combine::{Parser, ParserExt, char, parser};
+    use combine::{Parser, parser};
 
-    char('+')
-        .with(parser(signless_number))
-        .or(char('-')
-            .with(parser(signless_number))
-            .map(|n| -n))
-        .or(parser(signless_number))
-        .map(Number::from_bigrational)
+    signed!(parser(signless_number))
         .parse_state(input)
 }
 
 
-pub fn signless_number(input: &str) -> ParseResult<BigRational, &str>
+pub fn signless_number(input: &str) -> ParseResult<Number, &str>
 {
     use combine::{Parser, ParserExt, try, parser};
 
@@ -27,7 +43,8 @@ pub fn signless_number(input: &str) -> ParseResult<BigRational, &str>
         .parse_state(input)
 }
 
-pub fn zero_or_hexbin_number(input: &str) -> ParseResult<BigRational, &str>
+
+pub fn zero_or_hexbin_number(input: &str) -> ParseResult<Number, &str>
 {
     use combine::{
         Parser,
@@ -37,48 +54,47 @@ pub fn zero_or_hexbin_number(input: &str) -> ParseResult<BigRational, &str>
         many1,
         satisfy,
     };
-    use num::{BigInt, BigRational, Num};
+    use num::{BigInt, Num};
 
     char('0').with(
         char('x')
             .with(many1(hex_digit())
-                  .and_then(
-                      |s: String| BigInt::from_str_radix(s.as_str(), 16)))
+                  .and_then(from_radix!(BigInt, 16)))
             .or(
                 char('b')
                     .with(many1(satisfy(|c| c == '0' || c == '1'))
-                          .and_then(
-                              |s: String| BigInt::from_str_radix(s.as_str(), 2)))))
-        .map(BigRational::from_integer)
+                          .and_then(from_radix!(BigInt, 2)))))
+        .map(Number::from_bigint)
         .parse_state(input)
 }
 
-pub fn decimal_number(input: &str) -> ParseResult<BigRational, &str>
+pub fn decimal_number(input: &str) -> ParseResult<Number, &str>
 {
     use combine::{Parser, ParserExt, char, digit, optional, many1};
     many1(digit())
         .and(optional(char('.').with(many1(digit()))))
-        .and(optional(char('e').or(char('E')).with(many1(digit()))))
+        .and(
+            optional(
+                char('e').or(char('E'))
+                    .with(signed!(many1(digit())
+                                  .and_then(from_radix!(i32, 10))))))
         .and_then(
-            |((mut digs, optdec), optexp): ((String, Option<String>), Option<String>)| {
-                use num::{BigInt, FromPrimitive, Num, pow};
+            |((mut digs, optdec), optexp): ((String, Option<String>), Option<i32>)| {
+                use num::{BigInt, Num};
 
                 let decplaces = match optdec {
-                    None => 0,
+                    None => 0i32,
 
                     Some(dec) => {
                         digs.push_str(dec.as_str());
-                        dec.len()
+                        -(dec.len() as i32)
                     }
                 };
-                let exp = optexp.map(|s| s.parse::<usize>().unwrap()).unwrap_or(0);
+                let exp = optexp.unwrap_or(0);
                 let places = decplaces + exp;
-                let ten = BigInt::from_u64(10).unwrap();
-                let denom = pow(BigRational::from_integer(ten), places);
 
                 BigInt::from_str_radix(digs.as_str(), 10)
-                    .map(BigRational::from_integer)
-                    .map(|num| num / denom)
+                    .map(|i| Number::new(i, places))
             })
         .parse_state(input)
 }
--- src/value/number.rs
+++ src/value/number.rs
@@ -1,27 +1,158 @@
 use std::ops::Neg;
-use std::fmt::{Debug, Formatter, Error};
-use num::BigRational;
+use std::fmt::{Debug, Display, Formatter, Error};
+use num::{BigInt, FromPrimitive, Zero};
 
 
-#[derive(PartialEq)]
-#[derive(PartialOrd)]
-pub struct Number(BigRational);
+pub struct Number {
+    i: BigInt,
+    point: i32,
+}
 
 
 impl Number {
-    pub fn from_bigrational(r: BigRational) -> Number { Number(r) }
+    pub fn new(mut i: BigInt, mut point: i32) -> Number {
+        let ten: BigInt = BigInt::from_i64(10).unwrap();
+        let zero: BigInt = BigInt::zero();
+
+        // "Normalize point" to be the value closest to zero that
+        // preserves full information. For every trailing 0 digit in i,
+        // divide it by ten and increment point by 1.
+        //
+        // Examples:
+        //
+        // a: 1230000 pt -1 -> 123 pt 3
+        // b: 1230000 pt 2 -> 123 pt 6
+        // c: 1230000 pt -8 -> 123 pt -4
+        // d: -1230000 pt 2 -> -123 pt 6
+        //
+        if &i == &zero {
+            point = 0;
+        } else {
+            // FIXME: divmod operation may be an improvement.
+            while &i % &ten == zero {
+                i = &i / &ten;
+                point += 1;
+            }
+        }
+        Number {
+            i: i,
+            point: point,
+        }
+    }
+
+    pub fn from_bigint(i: BigInt) -> Number {
+        Number::new(i, 0)
+    }
 }
+
+
+impl PartialEq<Number> for Number {
+    fn eq(&self, other: &Number) -> bool {
+        self.point == other.point && self.i == other.i
+    }
+}
+
+
+// impl Add<Number> for Number {
+// }
+//
+//
+// impl Zero for Number {
+// fn zero()
+// }
+//
 
 
 impl Neg for Number {
     type Output = Number;
 
-    fn neg(self) -> Number { Number(-self.0) }
+    fn neg(self) -> Number {
+        Number::new(-self.i, self.point)
+    }
 }
 
 
 impl Debug for Number {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "Number {}", self.0)
+        write!(f, "Number {}", self)
     }
+}
+
+
+impl Display for Number {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        use num::Zero;
+
+        if self.i.is_zero() {
+            write!(f, "0")
+        } else {
+            let digs = format!("{}", self.i);
+            if self.point >= 0 {
+                try!(write!(f, "{}", digs));
+                for _ in 0..self.point {
+                    try!(write!(f, "0"));
+                }
+                Ok(())
+            } else {
+                let c = (digs.len() as i32) + self.point;
+                if c < 0 {
+                    use num::bigint::Sign;
+
+                    let signprefix = match self.i.sign() {
+                        Sign::NoSign => "",
+                        Sign::Minus => "-",
+                        Sign::Plus => "",
+                    };
+                    let signadj = signprefix.len();
+
+                    try!(write!(f, "{}0.", signprefix));
+                    for _ in 0..(-c + (signadj as i32)) {
+                        try!(write!(f, "0"));
+                    }
+                    write!(f, "{}", &digs[signadj..])
+                } else {
+                    let i = c as usize;
+                    write!(f, "{}.{}", &digs[..i], &digs[i..])
+                }
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::Number;
+
+    macro_rules! display_tests {
+        ( $( ( $name:ident, $i:expr, $pt:expr, $expected:expr ) ),* ) => {
+            $(
+                #[test]
+                fn $name() {
+                    use num::{BigInt, FromPrimitive};
+
+                    let s = format!("{}",
+                                    Number::new(
+                                        BigInt::from_i64($i).unwrap(),
+                                        $pt));
+                    assert_eq!(s, $expected);
+                }
+            )*
+        }
+    }
+
+    display_tests!((test_display_pos_pt_0, 3, 0, "3"),
+                   (test_display_pos_pt_pos, 3, 2, "300"),
+                   (test_display_pos_pt_negmid, 54321, -2, "543.21"),
+                   (test_display_pos_pt_negbig, 54321, -7, "0.0054321"),
+
+                   (test_display_neg_pt_0, -3, 0, "-3"),
+                   (test_display_neg_pt_pos, -3, 2, "-300"),
+                   (test_display_neg_pt_negmid, -54321, -2, "-543.21"),
+                   (test_display_neg_pt_negbig, -54321, -7, "-0.0054321"),
+
+                   (test_display_0_pt_0, 0, 0, "0"),
+                   (test_display_0_pt_pos, 0, 2, "0"),
+                   (test_display_0_pt_negmid, 0, -2, "0"),
+                   (test_display_0_pt_negbig, 0, -7, "0"));
 }
