0877b1d718f453b1935c72249769f7134df4decc
Nathan Wilcox <nathan@z.cash>
2018-07-04 05:56:20 -0700

    Change main to be a loop over lines.

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,13 @@ mod value;
 fn main() {
     use combine::{eof, parser, Parser};
     use parser::expr;
-    use std::io::{stdin, Read};
+    use std::io::BufRead;
 
-    let mut s = String::new();
-    stdin().read_to_string(&mut s).unwrap();
-    println!("input: {:?}", &s);
-    let result = parser(expr).skip(eof()).parse(&s);
-    println!("result: {:?}", &result);
+    let stdin = std::io::stdin();
+    for lineres in stdin.lock().lines() {
+        let line = lineres.unwrap();
+        println!("input: {:?}", &line);
+        let result = parser(expr).skip(eof()).parse(&line);
+        println!("result: {:?}", &result);
+    }
 }
