8b7c2830b4890828787ba7063a2d6b0c188f5662
Nathan Wilcox <nathan@z.cash>
2018-07-03 17:59:47 -0700

    [SKIP build] Unittest coverage with one case for each Atom.

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -19,5 +19,15 @@ pub fn expr(input: &str) -> ParseResult<Expr, &str> {
 mod tests {
     use super::expr;
 
-    test_cases_debugrepr_parser!(expr, [(test_expr_false, "false"), (test_expr_true, "true")]);
+    test_cases_debugrepr_parser!(
+        expr,
+        [
+            (test_expr_false, "false"),
+            (test_expr_true, "true"),
+            (test_expr_zero, "zero"),
+            (test_expr_char_x, "char_x"),
+            (test_expr_text_foo, "text_foo"),
+            (test_expr_symbol_sym, "symbol_sym")
+        ]
+    );
 }
