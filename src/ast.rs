2387fa5dadf8e6792436640c3cc9395d6e9a2dd1
Nathan Wilcox <nathan@z.cash>
2018-07-03 18:25:14 -0700

    [SKIP build] Add list expressions to the ast, basic whitespace-lacking parsing, a few unittests.

--- src/ast.rs
+++ src/ast.rs
@@ -3,4 +3,5 @@ use value::Atom;
 #[derive(Debug)]
 pub enum Expr {
     Atom(Atom),
+    List(Vec<Expr>),
 }
--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -2,15 +2,25 @@ use ast::Expr;
 use combine::{ParseResult, Parser};
 
 pub fn expr(input: &str) -> ParseResult<Expr, &str> {
-    use combine::parser;
+    use combine::char::char;
+    use combine::{between, parser, sep_end_by};
     use parser::atom;
 
-    parser(atom).map(Expr::Atom).parse_stream(input)
+    (parser(atom).map(Expr::Atom))
+        .or(between(char('['), char(']'), sep_end_by(parser(expr), char(','))).map(Expr::List))
+        .parse_stream(input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::expr;
 
-    test_cases_debugrepr_parser!(expr, [(test_expr_false, "false")]);
+    test_cases_debugrepr_parser!(
+        expr,
+        [
+            (test_expr_false, "false"),
+            (test_expr_list_empty, "list_empty"),
+            (test_expr_list_zero, "list_zero")
+        ]
+    );
 }
