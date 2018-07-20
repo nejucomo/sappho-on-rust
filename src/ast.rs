22caecd06c8e8bed2260e5391c6c5ecfa283ac6e
Nathan Wilcox <nathan@z.cash>
2018-07-20 09:48:23 -0700

    Remove an excessive layer in the type hierarchy, making AST flatter and more "direct".

--- src/ast.rs
+++ src/ast.rs
@@ -5,22 +5,13 @@ pub enum Expr {
     Atom(Atom),
     Deref(String),
     List(Vec<Expr>),
-    UnApp(UnaryApplication),
-    LookupApp(LookupApplication),
-    FuncApp(FunctionalApplication),
+    UnApp(UnaryOperator, Box<Expr>),
+    LookupApp(Box<Expr>, Symbol),
+    FuncApp(Box<Expr>, Box<Expr>),
 }
-
-#[derive(Clone, Debug)]
-pub struct UnaryApplication(pub UnaryOperator, pub Box<Expr>);
 
 #[derive(Clone, Debug)]
 pub enum UnaryOperator {
     Query,
     Mutate,
 }
-
-#[derive(Clone, Debug)]
-pub struct LookupApplication(pub Box<Expr>, pub Symbol);
-
-#[derive(Clone, Debug)]
-pub struct FunctionalApplication(pub Box<Expr>, pub Box<Expr>);
