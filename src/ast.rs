d7023ca3234876abc5be2a1536cebeb02ec40c0a
Nathan Wilcox <nejucomo@gmail.com>
2018-08-11 14:46:36 -0700

    [SKIP test] First stepping stone to an AST type refinement for func/query/proc.

--- src/ast.rs
+++ src/ast.rs
@@ -4,22 +4,50 @@ use value::{Atom, Symbol};
 pub struct Identifier(pub String);
 
 #[derive(Clone, Debug)]
-pub enum Expr {
+pub struct FuncExpr(pub GenExpr<FuncExpr>);
+
+/*
+#[derive(Clone, Debug)]
+pub enum QueryExpr {
+    GenExpr(GenExpr<QueryExpr>),
+    Query(Box<QueryExpr>),
+}
+*/
+
+#[derive(Clone, Debug)]
+pub enum ProcExpr {
+    GenExpr(GenExpr<SteppingStoneProcExpr>),
+    Query(Box<SteppingStoneProcExpr>),
+    Mutate(Box<SteppingStoneProcExpr>),
+}
+
+/* SteppingStoneProcExpr is a placeholder type we will remove later.
+ *
+ * We haven't updated the parser with the refinement, so every expression
+ * parses into this type. We introduce this to distinguish where we *should*
+ * parse GenExpr versus where we really should parse ProcExpr.
+ */
+#[derive(Clone, Debug)]
+pub struct SteppingStoneProcExpr(pub ProcExpr);
+
+#[derive(Clone, Debug)]
+pub enum GenExpr<X> {
     Atom(Atom),
     Deref(Identifier),
-    List(Vec<Expr>),
-    LookupApp(Box<Expr>, Symbol),
-    FuncApp(Box<Expr>, Box<Expr>),
-    UnApp(UnaryOperator, Box<Expr>),
-    BinOp(BinaryOperator, Box<Expr>, Box<Expr>),
+    List(Vec<X>),
+    LookupApp(Box<X>, Symbol),
+    FuncApp(Box<X>, Box<X>),
+    // UnApp(UnaryOperator, Box<X>),
+    BinOp(BinaryOperator, Box<X>, Box<X>),
     Lambda(LambdaDefinition),
 }
 
+/*
 #[derive(Clone, Debug)]
 pub enum UnaryOperator {
-    Query,
-    Mutate,
+    Invert,
 }
+*/
 
 #[derive(Clone, Debug)]
 pub enum BinaryOperator {
@@ -34,7 +62,7 @@ pub struct LambdaDefinition {
 }
 
 #[derive(Clone, Debug)]
-pub struct FunctionDefinition(pub Identifier, pub Box<Expr>);
+pub struct FunctionDefinition(pub Identifier, pub Box<SteppingStoneProcExpr>);
 
 impl From<FunctionDefinition> for LambdaDefinition {
     fn from(fd: FunctionDefinition) -> LambdaDefinition {
@@ -46,7 +74,7 @@ impl From<FunctionDefinition> for LambdaDefinition {
 }
 
 #[derive(Clone, Debug)]
-pub struct QueryDefinition(pub Box<Expr>);
+pub struct QueryDefinition(pub Box<SteppingStoneProcExpr>);
 
 impl From<QueryDefinition> for LambdaDefinition {
     fn from(qd: QueryDefinition) -> LambdaDefinition {
