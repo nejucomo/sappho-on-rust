77cf9518f19d89e30c7388b99528aedd28c94d1c
Nathan Wilcox <nejucomo@gmail.com>
2018-08-20 16:23:24 -0700

    Convert `from_radix` from macro to a function.

--- src/parser/atom/number.rs
+++ src/parser/atom/number.rs
@@ -1,12 +1,7 @@
 use combine::{ParseResult, Parser};
+use num::{BigInt, Num};
 use std::ops::Neg;
 use value::Number;
-
-macro_rules! from_radix {
-    ($t:ty, $radix:expr) => {
-        |s: String| <$t>::from_str_radix(s.as_str(), $radix)
-    };
-}
 
 pub fn number(input: &str) -> ParseResult<Number, &str> {
     use combine::{parser, Parser};
@@ -38,15 +33,12 @@ fn signless_number(input: &str) -> ParseResult<Number, &str> {
 fn zero_or_hexbin_number(input: &str) -> ParseResult<Number, &str> {
     use combine::char::{char, hex_digit};
     use combine::{many1, satisfy, Parser};
-    use num::{BigInt, Num};
 
     char('0')
         .with(
             char('x')
-                .with(many1(hex_digit()).and_then(from_radix!(BigInt, 16)))
-                .or(char('b').with(
-                    many1(satisfy(|c| c == '0' || c == '1')).and_then(from_radix!(BigInt, 2)),
-                )),
+                .with(from_radix(16, many1(hex_digit())))
+                .or(char('b').with(from_radix(2, many1(satisfy(|c| c == '0' || c == '1'))))),
         )
         .map(Number::from_bigint)
         .parse_stream(input)
@@ -61,11 +53,11 @@ fn decimal_number(input: &str) -> ParseResult<Number, &str> {
         .and(optional(
             char('e')
                 .or(char('E'))
-                .with(signed(many1(digit()).and_then(from_radix!(i32, 10)))),
+                .with(signed(from_radix(10, many1(digit())))),
         ))
         .and_then(
-            |((mut digs, optdec), optexp): ((String, Option<String>), Option<i32>)| {
-                use num::{BigInt, Num};
+            |((mut digs, optdec), optexp): ((String, Option<String>), Option<BigInt>)| {
+                use num::{BigInt, Num, ToPrimitive};
 
                 let decplaces = match optdec {
                     None => 0i32,
@@ -75,13 +67,20 @@ fn decimal_number(input: &str) -> ParseResult<Number, &str> {
                         -(dec.len() as i32)
                     }
                 };
-                let exp = optexp.unwrap_or(0);
+                let exp = optexp.and_then(|biref| biref.to_i32()).unwrap_or(0);
                 let places = decplaces + exp;
 
                 BigInt::from_str_radix(digs.as_str(), 10).map(|i| Number::new(i, places))
             },
         )
         .parse_stream(input)
+}
+
+fn from_radix<'a, P>(radix: u32, p: P) -> impl Clone + Parser<Output = BigInt, Input = &'a str>
+where
+    P: Clone + Parser<Output = String, Input = &'a str>,
+{
+    p.and_then(move |s: String| <BigInt>::from_str_radix(s.as_str(), radix))
 }
 
 #[cfg(tests)]
